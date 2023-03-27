21943af47566ce2174e919cf0373859b15d619fa
metinars <arsmetin90@gmail.com>
2023-03-27 05:35:05 +0300

    Writing and Reading Files

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
-const userName = "Metin";
+const fs = require('node:fs');
 
-console.log(`Hi ${userName}!`);
+fs.readFile('user-data.txt', (err, data) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log(data.toString());
+});
+
+fs.writeFile('user-data.txt', 'username=Metin', (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('Wrote to file!');
+  }
+});
