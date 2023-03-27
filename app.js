d1323584eb05a2bf5e123c817d77de1b0c97207b
metinars <arsmetin90@gmail.com>
2023-03-27 06:14:55 +0300

    Added First Server

--- app.js
+++ app.js
@@ -1,17 +1,8 @@
-const fs = require('node:fs');
+const http = require('http');
 
-fs.readFile('user-data.txt', (err, data) => {
-  if (err) {
-    console.log(err);
-    return;
-  }
-  console.log(data.toString());
+const server = http.createServer((request, response) => {
+  response.write('Hello there!');
+  response.end();
 });
 
-fs.writeFile('user-data.txt', 'username=Metin', (err) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log('Wrote to file!');
-  }
-});
+server.listen(3000);
